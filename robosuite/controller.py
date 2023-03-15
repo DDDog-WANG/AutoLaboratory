c832c4fdc545990743072b2e19f330427a384945
DDDog <superalpacaaa@icloud.com>
2023-03-15 22:01:51 +0900

    add robosuite folder

---
+++ robosuite/controller.py
@@ -0,0 +1,7 @@
+def init_controller(model,data):
+    #initialize the controller here. This function is called once, in the beginning
+    pass
+
+def controller(model, data):
+    #put the controller here. This function is called inside the simulation.
+    pass
---
+++ robosuite/own_Env.py
@@ -0,0 +1,48 @@
+from robosuite.models import MujocoWorldBase
+from robosuite.models.robots import Maholo
+from robosuite.models.robots import IIWA
+from robosuite.models.grippers import gripper_factory
+from robosuite.models.arenas import TableArena
+from robosuite.models.objects import BallObject
+from robosuite.utils.mjcf_utils import new_joint
+import mujoco
+import mujoco_viewer
+
+# Step 1: Creating the world. 
+world = MujocoWorldBase()
+
+# Step 2: Creating the robot, add a gripper.
+mujoco_robot = IIWA()
+# mujoco_robot.contact_geom_rgba([0.5, 0.5, 0.5, 1.0])
+# gripper = gripper_factory('PandaGripper')
+# mujoco_robot.add_gripper(gripper)
+mujoco_robot.set_base_xpos([0, 0, 0])
+world.merge(mujoco_robot)
+
+# Step 3: Creating the table.
+mujoco_arena = TableArena()
+mujoco_arena.set_origin([0.8, 0, 0])
+world.merge(mujoco_arena)
+
+# Step 4: Adding the object.
+sphere = BallObject(
+    name="sphere",
+    size=[0.04],
+    rgba=[0, 0.5, 0.5, 1]).get_obj()
+sphere.set('pos', '0.8 0 0.8')
+world.worldbody.append(sphere)
+
+# Step 5: Running Simulation.
+model = world.get_model(mode="mujoco")
+data = mujoco.MjData(model)
+
+# create the viewer object
+viewer = mujoco_viewer.MujocoViewer(model, data)
+
+# simulate and render
+for _ in range(100000):
+    mujoco.mj_step(model, data)
+    viewer.render()
+
+# close
+viewer.close()
