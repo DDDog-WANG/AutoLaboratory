a8eb5f333f465f3f7ec2433559bcd6d857fe5b54
DDDog-WANG <superalpacaaa@gmail.com>
2023-08-16 15:55:55 +0900

    collectdata scripts

--- scr/multiomics_keycontrol_joint.py
+++ scr/multiomics_keycontrol_joint.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 import robosuite as suite
 from robosuite import load_controller_config
 from robosuite.utils.transform_utils import quat2mat, mat2euler
-from pynput import keyboard
+# from pynput import keyboard
 from robosuite.wrappers.gym_wrapper import GymWrapper
 from sb3_contrib.common.wrappers import TimeFeatureWrapper
 
@@ -15,91 +15,96 @@ if __name__ == "__main__":
     parser.add_argument("--robots", type=str, default="Maholo")
     parser.add_argument("--camera", type=str, default="frontview")
     parser.add_argument("--video_name", type=str, default="my_video")
-    parser.add_argument("--t", type=int, default=1000)
     parser.add_argument("--horizon", type=int, default=1000)
     parser.add_argument("--height", type=int, default=1536)
     parser.add_argument("--width", type=int, default=2560)
     args = parser.parse_args()
 
-# controller_config = load_controller_config(default_controller="JOINT_POSITION")
-# env = suite.make(
-#     args.environment,
-#     args.robots,
-#     controller_configs=controller_config,
-#     has_renderer=True,
-#     has_offscreen_renderer=True,
-#     control_freq=50,
-#     render_camera=args.camera,
-#     camera_names=args.camera,
-#     camera_heights=args.height,
-#     camera_widths=args.width,
-# )
-
+controller_config = load_controller_config(default_controller="JOINT_POSITION")
 env = suite.make(
     args.environment,
     args.robots,
-    has_renderer=False,
-    has_offscreen_renderer=False,
+    controller_configs=controller_config,
+    has_renderer=True,
+    has_offscreen_renderer=True,
     use_camera_obs=False,
     control_freq=50,
     render_camera=args.camera,
-    render_gpu_device_id=0,
-    horizon=args.horizon,
+    camera_names=args.camera,
+    camera_heights=args.height,
+    camera_widths=args.width,
 )
+
+# env = suite.make(
+#     args.environment,
+#     args.robots,
+#     has_renderer=False,
+#     has_offscreen_renderer=False,
+#     use_camera_obs=False,
+#     control_freq=50,
+#     render_camera=args.camera,
+#     render_gpu_device_id=0,
+#     horizon=args.horizon,
+# )
 env = GymWrapper(env) 
 env = TimeFeatureWrapper(env)
 
-
-listener = keyboard.Listener()
 action = np.zeros(env.robots[0].dof)
+action = np.random.uniform(-1, 1, env.robots[0].dof)
 action_seq = []
 obs_seq = []
 reward_seq = []
-delta = 5
-key_li = ["0","1","2","3","4","5","6","7","8","~", "!",'"',"#","$","%","&","'","("]
-def on_press(key):
-    try:
-        for i in range(len(key_li)):
-            if i == 0:
-                if key.char == key_li[i]:
-                    action[0] = delta
-            elif i <= 8:
-                if key.char == key_li[i]:
-                    action[i+8] = delta
-            elif i == 9:
-                if key.char == key_li[i]:
-                    action[0] = -delta
-            elif i <= 17:
-                if key.char == key_li[i]:
-                    action[i-1] = -delta
-    except AttributeError:
-        pass
 
-def on_release(key):
-    try:
-        action[:] = 0
-    except AttributeError:
-        pass
+# delta = 5
+# key_li = ["0","1","2","3","4","5","6","7","8","~", "!",'"',"#","$","%","&","'","("]
+# listener = keyboard.Listener()
+# def on_press(key):
+#     try:
+#         for i in range(len(key_li)):
+#             if i == 0:
+#                 if key.char == key_li[i]:
+#                     action[0] = delta
+#             elif i <= 8:
+#                 if key.char == key_li[i]:
+#                     action[i+8] = delta
+#             elif i == 9:
+#                 if key.char == key_li[i]:
+#                     action[0] = -delta
+#             elif i <= 17:
+#                 if key.char == key_li[i]:
+#                     action[i-1] = -delta
+#     except AttributeError:
+#         pass
 
-listener = keyboard.Listener(on_press=on_press, on_release=on_release)
-listener.start()
+# def on_release(key):
+#     try:
+#         action[:] = 0
+#     except AttributeError:
+#         pass
 
-for n in tqdm(range(args.t)):
+# listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+# listener.start()
+obs = env.reset()
+for n in tqdm(range(args.horizon)):
+    obs_seq.append(obs)
     action_seq.append(action)
 
-    obs, reward, done, _ = env_wrap.step(action)
-    obs_wrap, reward_wrap, _, _ = env_wrap.step(action)
+    obs, reward, done, _ = env.step(action)
+    # obs_wrap, reward_wrap, _, _ = env.step(action)
+    reward_seq.append(reward)
 
-    obs_seq.append(obs_wrap)
-    reward_seq.append(reward_wrap)
-
-    env.render()
+    env.unwrapped.render()
 env.close()
 
 action_seq = np.array(action_seq)
-np.save("action_seq.npy", action_seq)
+print(action_seq.shape)
+np.save("./collectdata/action_seq.npy", action_seq)
+
 obs_seq = np.array(obs_seq)
-np.save("obs_seq.npy", obs_seq)
+print(obs_seq.shape)
+np.save("./collectdata/obs_seq.npy", obs_seq)
+
 reward_seq = np.array(reward_seq)
-np.save("reward_seq.npy", reward_seq)
+print(reward_seq.shape)
+np.save("./collectdata/reward_seq.npy", reward_seq)
 
