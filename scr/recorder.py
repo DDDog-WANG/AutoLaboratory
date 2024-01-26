4ec9b9bbe17f059b650a2c5faec20da8a4386612
DDDog-WANG <superalpacaaa@gmail.com>
2024-01-26 15:08:37 +0900

    upgrade start of 2024

--- scr/recorder.py
+++ scr/recorder.py
@@ -10,17 +10,17 @@ from robosuite.utils.transform_utils import quat2mat, mat2euler
 from multiomics_maholo_move import maholo_move
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--environment", type=str, default="MaholoLaboratory_eefR_Move2Tube")
+    parser.add_argument("--environment", type=str, default="MaholoLaboratory_eefR_Move2Pipette")
     parser.add_argument("--robots", type=str, default="Maholo")
     parser.add_argument("--controller", type=str, default="JOINT_VELOCITY")
     parser.add_argument("--camera", type=str, default="frontview")
     parser.add_argument("--video_name", type=str, default="video")
-    parser.add_argument("--t", type=int, default=100)
+    parser.add_argument("--t", type=int, default=50)
     parser.add_argument("--height", type=int, default=1536)
     parser.add_argument("--width", type=int, default=2560)
     args = parser.parse_args()
 
-writer = imageio.get_writer("./videos/"+args.environment+".mp4", fps=50)
+writer = imageio.get_writer("./videos/"+args.environment+"_"+args.controller+".mp4", fps=50)
 
 controller_config = load_controller_config(default_controller=args.controller)
 env = suite.make(
@@ -43,19 +43,21 @@ obs = env.reset()
 
 # action_seq = np.load("./collectdata/action_seq_total_OSC.npy")
 obs = env.reset()
-action = np.zeros(env.robots[0].dof)
-# for i in range(17):
-#     action = np.zeros(env.robots[0].dof)
-#     action[i] = 1
-#     print(i)
-for n in range(args.t):
-    # action = action_seq[n]
-    action = maholo_move(obs)
-    obs, reward, done, _ = env.step(action)
+dim = 14
+# dim = env.robots[0].dof
+action = np.zeros(dim)
+for i in range(dim):
+    action = np.zeros(dim)
+    action[i] = 1
+    print(i)
+    for n in range(args.t):
+        # action = action_seq[n]
+        # action = maholo_move(obs)
+        obs, reward, done, _ = env.step(action)
 
-    frame = obs[args.camera+"_image"]
-    frame = np.flip(frame, axis=0)
-    writer.append_data(frame)
+        frame = obs[args.camera+"_image"]
+        frame = np.flip(frame, axis=0)
+        writer.append_data(frame)
 env.close()
 writer.close()
 
