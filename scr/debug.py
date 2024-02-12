6d5a500a58078db0ddf5aef9f6650bb826fc62c8
DDDog-WANG <superalpacaaa@gmail.com>
2024-02-12 21:05:35 +0900

    renew abstract.png and README.md

--- scr/debug.py
+++ scr/debug.py
@@ -13,15 +13,15 @@ np.set_printoptions(precision=4, suppress=True)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--environment", type=str, default="MaholoLaboratory_eefR_Move2Tube")
+    parser.add_argument("--environment", type=str, default="MaholoLaboratory")
     parser.add_argument("--robots", type=str, default="Maholo")
     parser.add_argument("--controller", type=str, default="JOINT_POSITION")
+    parser.add_argument("--control_mode", type=str, default="8+7")
     parser.add_argument("--camera", type=str, default="frontview")
-    parser.add_argument("--video_name", type=str, default="my_video")
     parser.add_argument("--horizon", type=int, default=5000)
     parser.add_argument("--episode", type=int, default=1)
     parser.add_argument("--height", type=int, default=1536)
-    parser.add_argument("--width", type=int, default=2560)
+    parser.add_argument("--width", type=int, default=2560) 
     args = parser.parse_args()
 
 
@@ -29,15 +29,14 @@ controller_config = load_controller_config(default_controller=args.controller)
 env = suite.make(
     args.environment,
     args.robots,
-    # gripper_types=None,
     controller_configs=controller_config,
+    control_freq=50,
+    control_mode=args.control_mode,
     has_renderer=True,
-    renderer_config={"width": args.width, "height": args.height},
     has_offscreen_renderer=True,
+    render_camera=args.camera,
     use_object_obs=True,
     use_camera_obs=False,
-    control_freq=20,
-    render_camera=args.camera,
     camera_names=args.camera,
     camera_heights=args.height,
     camera_widths=args.width,
@@ -66,12 +65,12 @@ def print_joint_positions(joint_positions):
     print("pipette004_quat:", joint_positions[22:26])
     print("tube008_pos    :", joint_positions[26:29])
     print("tube008_quat   :", joint_positions[29:33])
+print_joint_positions(env.robots[0].sim.data.qpos)
 
 obs = env.reset()
 for key,value in obs.items():
     print(f"Key: {key}, Value.shape: {value.shape}")
 
-print_joint_positions(env.robots[0].sim.data.qpos)
 eef_pos = env.sim.data.get_body_xpos("gripper0_left_eef")
 eef_euler = env.sim.data.get_body_xquat("gripper0_left_eef")
 eef_pos = env.sim.data.get_body_xpos("gripper0_right_eef")
@@ -82,40 +81,57 @@ print("✣✣✢✢✢✢✢✢✢✢✢✢✢✢✢✢✢✢✢✢✢✢✢✢�
 print("✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✢✢✤✤")
 
 
-action = np.zeros(17)
-# action[16] = -1
-for n in range(100):
-    # action = np.random.uniform(-1, 1, size=env.robots[0].dof)
-    # action = action_seq[n]
-    # action = np.array([action[0]/0.0034, 
-    #                   action[1]/0.0033, action[2]/0.0032, action[3]/0.0032, action[4]/0.0033, action[5]/0.0031, action[6]/0.0029, action[7]/0.0029, action[8]/0.0001, 
-    #                   action[9]/0.0032, action[10]/0.0031, action[11]/0.0032, action[12]/0.0033, action[13]/0.0029, action[14]/0.0027, action[15]/0.0026, action[16]/0.0001])
-    # print(f"{n:03} {action}")
+for i in range(17):
+    action = np.zeros(17)
+    action[i] = 1
+    print(f"⭐️ axis: {i}, action: {action}")
+    for n in range(50):
 
-    # pre_joint_positions = joint_positions.copy()
-    obs, reward, done, _ = env.step(action)
-    # print("🔱", "{:03}".format(n), env.robots[0].sim.data.qpos[17:19], flush=True)
-    # print(action)
-    # print("🔱", "{:03}".format(n), "{:.5f}".format(reward), flush=True)
-    # print(np.linalg.norm(obs["g1_to_target_pos"]), obs["g1_to_target_quat"], obs["g1_to_target_pos"], flush=True)
-    # print(obs["pipette004_pos"], obs["pipette004_quat"], flush=True)
-    # print(obs["gripper1_to_pipette004"], obs["gripper1_quat_pipette004"], flush=True)
-    # joint_positions = env.robots[0].sim.data.qpos
-    # joint_positions = np.concatenate((joint_positions[:9],joint_positions[10:18]))
-    # # joint_positions = joint_positions[:8]
+        # action = np.random.uniform(-1, 1, 17)
+        # action = np.zeros(17)
 
-    # delta_joint_positions = joint_positions - pre_joint_positions
-    # action_seq_joint.append(delta_joint_positions)
-    # print(action/delta_joint_positions)
+        obs, reward, done, _ = env.step(action)
 
-    # cube_pos = env.sim.data.get_body_xpos("cube_main")
-    # cube_quat = env.sim.data.get_body_xquat("cube_main")
-    # cube_euler = mat2euler(quat2mat(cube_quat))
-    # print("cube: ",cube_pos, cube_euler)
-    env.render()
-    # env.unwrapped.render()
+        env.render()
+        # viewer.render()
 
 env.close()
+
+
+# action = np.zeros(17)
+# # action[16] = -1
+# for n in range(100):
+#     # action = np.random.uniform(-1, 1, size=env.robots[0].dof)
+#     # action = action_seq[n]
+#     # action = np.array([action[0]/0.0034, 
+#     #                   action[1]/0.0033, action[2]/0.0032, action[3]/0.0032, action[4]/0.0033, action[5]/0.0031, action[6]/0.0029, action[7]/0.0029, action[8]/0.0001, 
+#     #                   action[9]/0.0032, action[10]/0.0031, action[11]/0.0032, action[12]/0.0033, action[13]/0.0029, action[14]/0.0027, action[15]/0.0026, action[16]/0.0001])
+#     # print(f"{n:03} {action}")
+
+#     # pre_joint_positions = joint_positions.copy()
+#     obs, reward, done, _ = env.step(action)
+#     # print("🔱", "{:03}".format(n), env.robots[0].sim.data.qpos[17:19], flush=True)
+#     # print(action)
+#     # print("🔱", "{:03}".format(n), "{:.5f}".format(reward), flush=True)
+#     # print(np.linalg.norm(obs["g1_to_target_pos"]), obs["g1_to_target_quat"], obs["g1_to_target_pos"], flush=True)
+#     # print(obs["pipette004_pos"], obs["pipette004_quat"], flush=True)
+#     # print(obs["gripper1_to_pipette004"], obs["gripper1_quat_pipette004"], flush=True)
+#     # joint_positions = env.robots[0].sim.data.qpos
+#     # joint_positions = np.concatenate((joint_positions[:9],joint_positions[10:18]))
+#     # # joint_positions = joint_positions[:8]
+
+#     # delta_joint_positions = joint_positions - pre_joint_positions
+#     # action_seq_joint.append(delta_joint_positions)
+#     # print(action/delta_joint_positions)
+
+#     # cube_pos = env.sim.data.get_body_xpos("cube_main")
+#     # cube_quat = env.sim.data.get_body_xquat("cube_main")
+#     # cube_euler = mat2euler(quat2mat(cube_quat))
+#     # print("cube: ",cube_pos, cube_euler)
+#     env.render()
+#     # env.unwrapped.render()
+
+# env.close()
 
 
 # for key,value in obs.items():
